f0e2425cd588563a308adddb81d97677d462703c
MandaAlekhya <21311a05n0@sreenidhi.edu.in>
2024-06-11 14:21:10 +0530

    add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="index.css?version=51">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik+Moonrocks&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+</head>
+<style>
+ 
+</style>
+
+<body>
+
+   <div class="navbar" >
+        <div class="logotitle">
+            <a href="index.php" class="site-title">
+                Lost N Found
+            </a>
+        </div>
+        <ul>
+            <li>
+              <div class="dropdown">
+                <a  class="dropbtn">Lost</a>
+                <div class="dropcontent">
+                  <a style="font-size:20px" href="lost.php">Lost Items</a>
+                  <a style="font-size:20px" href="lost_c.php">Lost and Taken</a>
+                  <a style="font-size:20px" href="lost_order.php">Newest first</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="dropdown">
+                <a  class="dropbtn">Found</a>
+                <div class="dropcontent">
+                  <a style="font-size:20px" href="found.php">Found Items</a>
+                  <a style="font-size:20px" href="found_c.php">Found and Given</a>
+                  <a style="font-size:20px" href="found_order.php">Newest first</a>
+                </div>
+              </div>
+            </li>
+            <li>
+                <a href="google.php">Your Profile</a>
+            </li>
+            <li>
+                <a href="team.php">Team</a>
+            </li>   
+        </ul>
+    </div> 
+    <div class="container-fluid">
+  <img class="image" src="llnf.jpg" alt="">
+
+  <div class="bodydiv">
+    <h1 class="bodyh1">LOST </h1>
+    <h1 class="bodyh1">SOMETHING? </h1>
+    <h1 class="bodyh1">FOUND </h1>
+  <h1 class="bodyh1">SOMETHING? </h1>
+  </div>
+</div> 
+</body>
+</html>
